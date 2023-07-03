d644ce7902bdd8aaf7c54e44847a6db99a397e63
Amaymani <amaytripathi19@gmail.com>
2023-07-03 16:31:52 +0530

    bcrypt security added

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const encrypt= require("mongoose-encryption");
+const bcrypt = require('bcrypt');
 
+const saltRounds = 10;
 const app = express();
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
@@ -22,32 +24,39 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post("/register", function (req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
-  });
 
-  newUser.save().then(async function (data, err) {
-    if (err) {
-        console.error(err);
-    }
-    else {
-        res.render("secrets");
-    }
-  });
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+  
+    newUser.save().then(async function (data, err) {
+      if (err) {
+          console.error(err);
+      }
+      else {
+          res.render("secrets");
+      }
+    });
+});
+  
 });
 
 app.post("/login", function(req,res){
+  
     const username= req.body.username;
-    const password= req.body.password;
+    const password= req.body.password ;
 
     User.findOne({email: username}).then(async function(foundUser, err){
         if (err){
             console.log(err);
         } else{
-            if(foundUser.password===password){
-                res.render('secrets');
+          bcrypt.compare(password, foundUser.password, function(err, result) {
+            if(result===true){
+              res.render("secrets");
             }
+        });
         }
     })
 });
